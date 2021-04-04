8fead03e63d35d3cb279e7c7d30ad6e5a92ab3c6
nehak6296 <nehak6296@gmail.com>
2021-04-04 18:17:31 +0530

    Added Indian State census data

---
+++ IndianStatesCensusAnalyzer/IndianStatesCensusAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStatesCensusAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
